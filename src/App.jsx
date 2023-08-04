6ccf7e6a3ad84a0c0e56a72a261d3f6c4641230e
Diptiranjan <iamdiptiranjan2004@gmail.com>
2023-08-04 16:05:48 +0530

    Starting a 3d Project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  Starcanvas,
+} from "./components";
+
+const App = () => {
+  return <></>;
+};
+
+export default App;
